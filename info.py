5004234c8fff53709192c124def45763473c7b46
Ak all bots Control <akallbotscontrol@gmail.com>
2025-08-15 15:01:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -97,7 +97,7 @@ OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/MR_ABHAY_K')
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
 PM_SEARCH = bool(environ.get('PM_SEARCH', False))
 BUTTON_MODE = bool(environ.get('BUTTON_MODE', False))
-MAX_BTN = bool(environ.get('MAX_BTN', True))
+MAX_BTN = bool(environ.get('MAX_BTN', False))
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 IMDB = bool(environ.get('IMDB', False))
 AUTO_FFILTER = bool(environ.get('AUTO_FFILTER', True))
@@ -196,3 +196,4 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
